8992b1c50c8a7d5403e7b2eb39c7fe024ea44cbf
David <david.gwynne@ntlworld.com>
2019-11-20 11:52:01 +0000

    added postman files

---
+++ subPost.sh
@@ -0,0 +1,10 @@
+curl -X POST {your-callback-url} -H ‘Content-Type: application/json’
+
+ -d ‘{
+      “aspect_type”: “create”,
+      “event_time”: 1549560669,
+      “object_id”: 0000000000,
+      “object_type”: “activity”,
+      “owner_id”: 1088407,
+      “subscription_id”: 146920,
+    }’
