4ec9dff053636d6d186e7bb881b2b77240b4e2d3
David <david.gwynne@ntlworld.com>
2019-11-26 16:04:46 +0000

    added new webpage

--- backend/soup.py
+++ backend/soup.py
@@ -7,12 +7,13 @@ conn = sqlite3.connect('scrape.db')
 c = conn.cursor()
 
 # Open webpage
-url = "https://www.wiggle.co.uk/cycle/clothing/"
-page = requests.get(url)
+baseURL = "https://www.wiggle.co.uk/cycle/clothing/?g=" # base url ?g=1
 
-
-
-
+# ?g=97 (48+1)
+for i in range(1): #126 possible
+	url = baseURL + str(i*48+1)
+	page = requests.get(url)
+	
 
 
 
