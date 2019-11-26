2cc1e2f5c5d624ff45f3c5c9b878dc73fe5b7f35
David <david.gwynne@ntlworld.com>
2019-11-26 16:17:07 +0000

    navigating main clothing page

--- backend/soup.py
+++ backend/soup.py
@@ -12,8 +12,14 @@ baseURL = "https://www.wiggle.co.uk/cycle/clothing/?g=" # base url ?g=1
 # ?g=97 (48+1)
 for i in range(1): #126 possible
 	url = baseURL + str(i*48+1)
-	page = requests.get(url)
-	
-
-
+	page = requests.get(url)	
+	soup = bs(page.text, 'html.parser')
+	"""
+	productDivs = soup.findAll("a", {"class": "bem-product-thumb__image-link--grid"})
+	for div in productDivs:
+		print div.find('a')['href']
+	"""
+	productLinks = [div.a for div in soup.findAll('div', attrs={'class' : "bem-product-thumb--grid"})]
+	for link in productLinks:
+		print(link['href'])
 
