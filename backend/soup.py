be14378d8893a9d861a5b03c88317302fb4dcf8f
David <david.gwynne@ntlworld.com>
2019-11-26 15:52:53 +0000

    added requests

--- backend/soup.py
+++ backend/soup.py
@@ -1,4 +1,17 @@
 import sqlite3
+import requests
 from bs4 import BeautifulSoup as bs
 
-print("worked")
+# Open Database
+conn = sqlite3.connect('scrape.db')
+c = conn.cursor()
+
+url = "https://www.wiggle.co.uk/cycle/clothing/"
+page = requests.get(url)
+
+
+
+
+
+
+
