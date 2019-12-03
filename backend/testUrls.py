86876bb0ac0ad0bac86cc1062479bb32da2e1f6a
David <david.gwynne@ntlworld.com>
2019-12-03 09:03:35 +0000

    all urls correct

--- backend/testUrls.py
+++ backend/testUrls.py
@@ -9,4 +9,14 @@ items=c.fetchall()
 items=[i[0] for i in items]
 items = [e.replace(" ", "-") for e in items]
 
-print(items)
+failedList = []
+
+def testURL(category):
+	url = "https://www.wiggle.co.uk/cycle/"+category
+	r=requests.get(url)
+	if r.status_code != 200:
+		failedList.append(category)
+
+[testURL(i) for i in items]
+
+print(failedList)
