4a605fe1010900cecc2e6da0d4eefa746037fa4a
David <david.gwynne@ntlworld.com>
2019-12-11 12:06:43 +0000

    list comprehensions

--- backend/getItems.py
+++ backend/getItems.py
@@ -11,12 +11,12 @@ c = conn.cursor()
 
 c.execute("""SELECT item,URL FROM mainCategories;""")
 urls=c.fetchall()
+
+pages = ["/?g="+str(i*48+1) for i in range(5)] #/?g=1, /?g=49, /?g=97...
+cats=[i[0] for i in urls]
 urls=[i[1] for i in urls]
-print(urls)
-"""
-?g=49
-?g=97
-?g=145
-?g=193
-"""
-pages = ["/?g="+str(i*48+1) for i in range(100)] #/?g=1, /?g=49, /?g=97....
+
+longUrls=[i+j for i in urls for j in pages]
+
+print(longUrls)
+
