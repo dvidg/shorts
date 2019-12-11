b48770495a120ca4956f7ecf78881124d844f8be
David <david.gwynne@ntlworld.com>
2019-12-11 13:47:08 +0000

    access minimum number of URLs

--- backend/getItems.py
+++ backend/getItems.py
@@ -12,7 +12,8 @@ c = conn.cursor()
 c.execute("""SELECT item,URL FROM mainCategories;""")
 urls=c.fetchall()
 
-pages = ["/?g="+str(i*48+1) for i in range(5)] #/?g=1, /?g=49, /?g=97...
+itemNumber = [i*48+1 for i in range(50)]
+pages = ["/?g="+str(i) for i in itemNumber] #/?g=1, /?g=49, /?g=97...
 cats=[i[0] for i in urls]
 urls=[i[1] for i in urls]
 dictionary={}
@@ -22,12 +23,12 @@ for i in [urls[-1]]: #[urls[-1]] for testing
 	page = requests.get(i)
 	soup = bs(page.text, "html.parser")
 	itemNumString = soup.find("div", {"class":"bem-paginator__text-block"}).text	
-	numItems = int(itemNumString.strip().split()[-1])# number of items
-	print(numItems)
-"""	
-	longUrls = [i+j for j in pages]
+	numItems = int(itemNumString.strip().split()[-1]) # number of items
+
+	# lastCat
+	lastCats = [i for i in itemNumber if i > numItems]
+	longUrls = [i+j for j in [pages[-len(lastCats)-1]]]
 	dictionary[cats[0]] = longUrls
 	cats.pop(0)
-"""
 
-# <div class=""bem-noresults__alert-text"></div>
+
