6e4270f4350cc0b95284598dbc3347313848573a
David <david.gwynne@ntlworld.com>
2019-12-11 11:22:39 +0000

    new url setup

--- backend/getItems.py
+++ backend/getItems.py
@@ -9,7 +9,14 @@ from bs4 import BeautifulSoup as bs
 conn = sqlite3.connect('scrape.db')
 c = conn.cursor()
 
-c.execute("""SELECT URL FROM mainCategories;""")
+c.execute("""SELECT item,URL FROM mainCategories;""")
 urls=c.fetchall()
-urls=[i[0] for i in urls]
+urls=[i[1] for i in urls]
 print(urls)
+"""
+?g=49
+?g=97
+?g=145
+?g=193
+"""
+pages = ["/?g="+str(i*48+1) for i in range(100)] #/?g=1, /?g=49, /?g=97....
