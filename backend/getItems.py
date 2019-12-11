29762aaaf7f3f9b1bf21facd7782a3d9bab54e38
David <david.gwynne@ntlworld.com>
2019-12-11 13:17:47 +0000

    now getting item number

--- backend/getItems.py
+++ backend/getItems.py
@@ -17,8 +17,17 @@ cats=[i[0] for i in urls]
 urls=[i[1] for i in urls]
 dictionary={}
 
-for i in urls:
+for i in [urls[-1]]: #[urls[-1]] for testing
+	# i is a https://www.wiggle.co.uk/cycle/Beanies
+	page = requests.get(i)
+	soup = bs(page.text, "html.parser")
+	itemNumString = soup.find("div", {"class":"bem-paginator__text-block"}).text	
+	numItems = itemNumString.strip().split()[-1] # number of items
+	print(numItems)
+"""	
 	longUrls = [i+j for j in pages]
 	dictionary[cats[0]] = longUrls
 	cats.pop(0)
+"""
 
+# <div class=""bem-noresults__alert-text"></div>
