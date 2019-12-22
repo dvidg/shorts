f64294ad4166b842481d0efe936cbf40ee9eb92b
David <dpg32@bath.ac.uk>
2019-12-22 19:10:14 +0000

    encapsulation

--- backend/getItemURLs.py
+++ backend/getItemURLs.py
@@ -12,15 +12,26 @@ c = conn.cursor()
 
 #c.execute("""INSERT INTO categoryURLs (category, URLs) VALUES (?, ?)""", (cats[0],str(longUrls).strip('[]')))
 
+def getURL(localURL):
+	page = requests.get(url)	
+	soup = bs(page.text, 'html.parser')
+	productLinks = [img.src for a in soup.findAll('a', attrs={'class' : "bem-product-thumb__image-link--grid"})]
+	print(productLinks)
+
+
+
+
 
 c.execute("""SELECT category FROM categoryURLs;""")
 cats=[i[0] for i in c.fetchall()]
 
 # Remove [] around cats when moving away from single testing
-for category in [cats[2]]:
+for category in cats:
 	c.execute("""SELECT URLs FROM categoryURLs WHERE category=?""", (category,))
 	urls = ast.literal_eval([i[0] for i in c.fetchall()][0])
 	
+	if (category == "Baggy-Shorts"):
 
+		
 
 conn.commit()	
